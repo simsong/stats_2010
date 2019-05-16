21ba2bcfb48303a3cf893d83e6413e5e81d31531
Simson L. Garfinkel <simsong@acm.org>
2019-05-15 19:36:43 -0700

    checkpoint

--- recon/s2_nbuild_state_stats.py
+++ recon/s2_nbuild_state_stats.py
@@ -25,6 +25,7 @@ sys.path.append( os.path.join(os.path.dirname(__file__),".."))
 
 from total_size import total_size
 import dbrecon
+from dbrecon import dopen
 from ctools.timer import Timer
 
 def sf1_zipfilename(state_abbr):
@@ -33,7 +34,7 @@ def sf1_zipfilename(state_abbr):
 def open_segment(state_abbr,segment):
     """ Given a state abbreviation and a segment number, open it"""
     sf1_input_file = f"{state_abbr}{segment:05d}2010.sf1"
-    return dbrecon.dopen(sf1_input_file, zipfilename=sf1_zipfilename(state_abbr), encoding='latin1')
+    return dopen(sf1_input_file, zipfilename=sf1_zipfilename(state_abbr), encoding='latin1')
 
 def process_state(state_abbr):
     logging.info(f"{state_abbr}: building data frame with all SF1 measurements")
@@ -42,9 +43,10 @@ def process_state(state_abbr):
         # Read in layouts -- they are json created from xsd from the access database
         # on the website.  Note -- the xsd had to be modified to undo the file
         # mods due to access limitations.  It's read as a ordered dict to preserve
-        # the order of the layout to read the csv.
+        # the order of the layouts to read the csv.
 
-        layout           = json.load(dbrecon.dopen('$SRC/layouts/layouts.json'), object_pairs_hook=OrderedDict)
+        state_abbr_upper = state_abbr.upper()
+        layouts           = json.load(dopen('$SRC/layouts/layouts.json'), object_pairs_hook=OrderedDict)
         geo_filename     = f"$ROOT/{state_abbr}/geofile_{state_abbr}.csv"
         done_filename    = f"$ROOT/{state_abbr}/completed_{state_abbr}_02"
 
@@ -55,11 +57,12 @@ def process_state(state_abbr):
                  and dbrecon.dpath_exists(done_filename)):
                 dbrecon.dpath_unlink(done_filename)
 
-        # If done_file exists, we don't need to run. This would be better done by checking all of the county files.
+        # If done_file exists, we don't need to run.
+        # This would be better done by checking all of the county files.
+
         if dbrecon.dpath_exists(done_filename):
             print(f"{state_abbr} already exists")
             return
-
 
         # Generate the CSV header that the original code used
         # This looks weird, but we are trying to match the original files exactly.
@@ -68,7 +71,7 @@ def process_state(state_abbr):
         segment_numbers = []
         for i in range(1,48):
             try:
-                heads = layout[f"SF1_{i:05d}.xsd"]
+                heads = layouts[f"SF1_{i:05d}.xsd"]
                 [field_names.append(head+"_"+xy) for head in heads[0:4]]
                 if i==1:
                     assert heads[4]=='LOGRECNO'
@@ -78,8 +81,9 @@ def process_state(state_abbr):
                 segment_numbers.append(i) # we are using these segments only
             except KeyError as e:
                 pass
-        [field_names.append(head) for head in "STATE,COUNTY,TRACT,BLOCK,BLKGRP,SUMLEV,geoid".split(",")]
-
+        JOIN_FIELDS = "STATE,COUNTY,TRACT,BLOCK,BLKGRP,SUMLEV".split(",")
+        [field_names.append(field) for field in JOIN_FIELDS]
+        field_names.append("geoid")
 
         # The SF1 directory consists of 47 or 48 segments. The first columns are defined below:
         # Summary levels at https://factfinder.census.gov/help/en/summary_level_code_list.htm
@@ -87,41 +91,81 @@ def process_state(state_abbr):
         SUMLEV_TRACT  = '140' # State-County-Census Tract
         SUMLEV_BLOCK  = '101' # State-County-County Sub∀division-Place/Remainder-Census Tract∀-Block Group-Block
         SF1_LINKAGE_VARIABLES = ['FILEID','STUSAB','CHARITER','CIFSN','LOGRECNO']
-        SUMLEV_FIELD  = 2       # summary level is the 3rd field in the geoheader
-        LOGRECNO_FIELD= 6       # logrecno field is the 7th field
 
-        # Track the LOGRECNO values for each geolevel we care about
-        logrecno_for_geolevel = {SUMLEV_COUNTY:[],
-                                 SUMLEV_TRACT:[],
-                                 SUMLEV_BLOCK:[] }
+        geo_fields = {a[1]:a[0] for a in enumerate(layouts['GEO_HEADER_SF1.xsd'])}
+        assert geo_fields['FILEID']==0 and geo_fields['STUSAB']==1
+
+        GEO_SUMLEV_FIELD  = geo_fields['SUMLEV']
+        GEO_LOGRECNO_FIELD= geo_fields['LOGRECNO']
+        GEO_STATE_FIELD   = geo_fields['STATE']
+        GEO_COUNTY_FIELD  = geo_fields['COUNTY']
+        GEO_TRACT_FIELD  = geo_fields['TRACT']
+        GEO_BLOCK_FIELD  = geo_fields['BLOCK']
 
         # Track the logical record for the join (we really don't need the whole field)
         logrecs = {}
+        from collections import defaultdict
+        counts = defaultdict(int)
 
-        with dbrecon.dopen(geo_filename,"r") as f:
+        with dopen(geo_filename,"r") as f:
+            ct = 0
             for line in f:
                 geo_data = line.split(",")
-                sumlev   = geo_data[SUMLEV_FIELD]
-                logrecno = geo_data[LOGRECNO_FIELD]
-                if sumlev in logrecno_for_geolevel:
-                    logrecno_for_geolevel[ sumlev ].append( logrecno )
-                    logrecs[logrecno] = geo_data
+                sumlev   = geo_data[GEO_SUMLEV_FIELD]
+                if sumlev in [SUMLEV_COUNTY,SUMLEV_TRACT,SUMLEV_BLOCK]:
+                    ct += 1
+                    logrecno = geo_data[GEO_LOGRECNO_FIELD]
+                    county   = geo_data[GEO_COUNTY_FIELD]
+                    geocode  = "".join(geo_data[f] for f in
+                                       [GEO_STATE_FIELD,GEO_COUNTY_FIELD,GEO_TRACT_FIELD,GEO_BLOCK_FIELD])
+                    logrecs[logrecno] = (sumlev, ct, county,
+                                         [geo_data[ geo_fields[ field ] ] for field in JOIN_FIELDS],
+                                         geocode )
+                    counts[sumlev] += 1
                 
-        print("{} geography file processed. counties:{}  tracts:{}  blocks:{}  mem:{:,}+{:,}={:,}".format(
-            state_abbr,
-            len(logrecno_for_geolevel[SUMLEV_COUNTY]),
-            len(logrecno_for_geolevel[SUMLEV_TRACT]),
-            len(logrecno_for_geolevel[SUMLEV_BLOCK]),
-            total_size(logrecno_for_geolevel),
-            total_size(logrecs),
-            total_size(logrecno_for_geolevel) + total_size(logrecs)))
-
+        print("{} geography file processed. counties:{}  tracts:{}  blocks:{}  mem:{:,}".format(
+            state_abbr, counts[SUMLEV_COUNTY], counts[SUMLEV_TRACT], counts[SUMLEV_BLOCK],
+            total_size(logrecs)))
 
         # Open the geofile and find all of the LOGRECNOs for the summary summary levels we care about
         # We are splitting on the entire line, which is a waste. But most of the data we are collecting is at the
         # block level, so most of the time we aren't wasting the work
-        open_segments = [open_segment(state_abbr,segment) for segment in segment_numbers]
+        open_segment_files = [open_segment(state_abbr,segment) for segment in segment_numbers]
 
+        # Open the block, tract, and county files for every county
+        # and write the first line
+        output_files = {}
+        state_code = dbrecon.state_fips(state_abbr)
+        for county_code in dbrecon.counties_for_state(state_abbr):
+            countydir = f'$ROOT/{state_abbr}/{state_code}{county_code}'
+            dbrecon.dmakedirs(countydir)
+            output_files[county_code] = {
+                SUMLEV_COUNTY: dopen(f'{countydir}/sf1_county_{state_code}{county_code}.csv','w') ,
+                SUMLEV_BLOCK: dopen(f'{countydir}/sf1_block_{state_code}{county_code}.csv','w') ,
+                SUMLEV_TRACT: dopen(f'{countydir}/sf1_tract_{state_code}{county_code}.csv','w') }
+            for f in output_files[county_code].values():
+                f.write(",".join(field_names))
+                f.write("\n")
+
+        # Now, for each segment, grab the fields, join, and stuff...
+
+
+        ct = 0
+        while True:
+            line = ",".join([f.readline().strip() for f in open_segment_files])
+            fields = line.split(",")
+            if fields[0]=='':   # end of file!
+                break
+            if fields[0]!='SF1ST' or fields[1]!=state_abbr_upper:
+                raise RuntimeError(f"bad fields in line {ct}: {fields}")
+            logrecno = fields[4]
+            try:
+                (sumlev,ct,county,joinfields,geocode) = logrecs[logrecno]
+                outline = str(ct)+","+(",".join(fields+joinfields)) + "," + geocode + "\n"
+                output_files[county][sumlev].write(outline)
+                ct += 1
+            except KeyError:
+                pass
 
 
 

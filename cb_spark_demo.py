4b9ec8941ca4556a1df7397840320a093ea417ca
Simson L. Garfinkel <simson.l.garfinkel@census.gov>
2019-07-25 18:05:47 -0400

    Continued expanding smallCells fxn. Encountered lots of bugs in parsed 2000 SF1 PDF-to-csv file. Started manually editing it to try to fix, made a copy with my name on it to distinguish.

--- cb_spark_demo.py
+++ cb_spark_demo.py
@@ -28,61 +28,78 @@ else:
 def smallCellStructure_PersonsSF2000():
     # From a list of tables with integer counts, find those with small counts in 2000 SF1
     # See Ch. 6 of https://www.census.gov/prod/cen2000/doc/sf1.pdf
-    tables = [  
-                "P3",       # Race                          [[[Block level tables begin]]]
-                "P4",       # HISP x Race
-                "P5",       # Race for Pop 18+
-                "P6",       # HISP x Race for Pop 18+
-                "P12",      # Sex by Age (2-5 year buckets)
-                "P14",      # Sex by Age for Pop <20
-                "P16",      # Pop in HHs
-                # P27-P30, P32, P36; REL not yet in schema
-                "P37",      # Pop in GQs
-                "P38",      # Pop in GQs by Sex by Age
-                "P12A-F",   # Sex by Age (Major Race Alone)
-                "P12G",     # Sex by Age (2+ races)
-                "P12H",     # Sex by Age (HISP)
-                "P12I",     # Sex by Age (White Alone, Not HISP)
-                # P27A-P30I, P32A-I; REL not yet in schema
-                "PCT12",    # Sex by Age (1 year buckets)   [[[Tract level tables begin]]]
-                "PCT13",    # Sex by Age, Pop in HHs
-                # PCT16-PCT17; 3-digit GQs not yet in schema
-                "PCT12A-F", # Sex by Age (Major Race Alone)
-                "PCT12G",   # Sex by Age (2+ races)
-                "PCT12H",   # Sex by Age (HISP)
-                "PCT12I-N", # Sex by Age (Major Race Alone, Not HISP)
-                "PCT12O",   # Sex by Age (2+ races, Not HISP)
-                "PCT13A-I", # Sex by Age (Major Race Alone), Pop in HHs
-                "PCT13G",   # Sex by Age (2+ races), Pop in HHs
-                "PCT13H",   # Sex by Age (HISP), Pop in HHs
-                "PCT13I"    # Sex by Age (White Alone, Not HISP), Pop in HHs
-                # PCT17A-I; 3-digit GQs not yet in schema
-             ]
+    from string import ascii_uppercase
+    tables =    [  
+                    "P3",       # Race                          [[[Block level tables begin]]]
+                    "P4",       # HISP x Race
+                    "P5",       # Race for Pop 18+
+                    "P6",       # HISP x Race for Pop 18+
+                    "P12",      # Sex by Age (2-5 year buckets)
+                    "P14",      # Sex by Age for Pop <20
+                    "P16",      # Pop in HHs
+                    # P27-P30, P32, P36; REL not yet in schema
+                    "P37",      # Pop in GQs
+                    "P38"       # Pop in GQs by Sex by Age                
+                ]
+    # Sex by Age (Major Race Alone)
+    tables +=   [f"P12{letter}" for letter in ascii_uppercase[:6]] # A-F
+    tables +=   [
+                    "P12G",     # Sex by Age (2+ races)
+                    "P12H",     # Sex by Age (HISP)
+                    "P12I",     # Sex by Age (White Alone, Not HISP)
+                    # P27A-P30I, P32A-I; REL not yet in schema
+                    "PCT12",    # Sex by Age (1 year buckets)   [[[Tract level tables begin]]]
+                    "PCT13"     # Sex by Age, Pop in HHs
+                    # PCT16-PCT17; 3-digit GQs not yet in schema
+                ]
+    # Sex by Age (Major Race Alone)
+    tables +=   [f"PCT12{letter}" for letter in ascii_uppercase[:6]] # A-F
+    tables +=   [
+                    "PCT12G",   # Sex by Age (2+ races)
+                    "PCT12H",   # Sex by Age (HISP)
+                ]
+    # Sex by Age (Major Race Alone, Not HISP)
+    tables +=   [f"PCT12" for letter in ascii_uppercase[8:8+6]] # I-N 
+    tables +=   ["PCT12O"]      # Sex by Age (2+ races, Not HISP)
+    # Sex by Age (Major Race Alone), Pop in HHs
+    tables +=   [f"PCT13{letter}" for letter in ascii_uppercase[:9]] # A-I
+    tables +=   [
+                    "PCT13G",   # Sex by Age (2+ races), Pop in HHs
+                    "PCT13H",   # Sex by Age (HISP), Pop in HHs
+                    "PCT13I"    # Sex by Age (White Alone, Not HISP), Pop in HHs
+                    # PCT17A-I; 3-digit GQs not yet in schema
+                ]
 
     year = 2000
     product = SF1
-    sf1_2000 = cb_spec_decoder.DecennialData(dataroot=DATAROOT, year=year,product=product)
+    sf1_2000 = cb_spec_decoder.DecennialData(dataroot=DATAROOT, year=year, product=product)
 
     print("Geolevels by state and summary level:")
     sf1_2000.get_df(tableName = GEO_TABLE, sqlName='GEO_2000')
 
-    print("Table P2 just has counts. Here we dump the first 10 records:")
-    sf1_2000.get_df(tableName="P2", sqlName='P2_2000')
-    d1 = spark.sql("SELECT * FROM P2_2000 LIMIT 10")
-    d1.show()
-    sf1_2000.print_legend(d1)
+    for table in tables:
+        print(f"Table {table} just has counts. Here we dump the first 10 records:")
+        sf1_2000.get_df(tableName="{table}", sqlName='{table}_2000')
+        d1 = spark.sql("SELECT * FROM {table}_2000 LIMIT 10")
+        d1.show()
+        sf1_2000.print_legend(d1)
 
-    print("Table P2 counts by state and county:")
-    res = spark.sql("SELECT GEO_2000.STUSAB,GEO_2000.COUNTY,GEO_2000.NAME,P0020001,P0020002,P0020003,P0020004,P0020005,P0020006 FROM GEO_2000 "
-                    "INNER JOIN P2_2000 ON GEO_2000.STUSAB=P2_2000.STUSAB and GEO_2000.LOGRECNO=P2_2000.LOGRECNO "
-                    "WHERE GEO_2000.SUMLEV='050' ORDER BY STUSAB,COUNTYCC")
+        print(f"Table {table} contains the following variables:")
+        print(sf1_2000.get_table(table).varnames + '\n')
 
-    tt = tydoc.tytable()
-    tt.add_head( res.columns )
-    for row in res.collect():
-        tt.add_data(row)
-    tt.render(sys.stdout, format='md')
-    sf1_2000.print_legend(res)
+        """
+        print(f"Table {table} counts by state and county:")
+        res = spark.sql("SELECT GEO_2000.STUSAB,GEO_2000.COUNTY,GEO_2000.NAME,P0020001,P0020002,P0020003,P0020004,P0020005,P0020006 FROM GEO_2000 "
+                        "INNER JOIN {table}_2000 ON GEO_2000.STUSAB={table}_2000.STUSAB and GEO_2000.LOGRECNO={table}_2000.LOGRECNO "
+                        "WHERE GEO_2000.SUMLEV='050' ORDER BY STUSAB,COUNTYCC")
+
+        tt = tydoc.tytable()
+        tt.add_head( res.columns )
+        for row in res.collect():
+            tt.add_data(row)
+        tt.render(sys.stdout, format='md')
+        sf1_2000.print_legend(res)
+        """
 
 def demo():
     # Get a schema object associated with the SF1 for 2010
@@ -176,4 +193,6 @@ if __name__=="__main__":
     # Note that getting spark here causes the arguments to be reparsed under spark-submit
     # Normally that's not a problem
     spark  = cspark.spark_session(logLevel='ERROR', pydirs=['.','ctools','ctools/schema'])
-    demo()
+    #demo()
+    smallCellStructure_PersonsSF2000()
+

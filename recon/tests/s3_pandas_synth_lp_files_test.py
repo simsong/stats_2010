2c5ffd527c51d616d6d21b6e1e4fe8cf70a2aeb1
Simson Garfinkel <simsong@acm.org>
2019-08-04 23:02:53 -0400

    added checker

--- recon/tests/s3_pandas_synth_lp_files_test.py
+++ recon/tests/s3_pandas_synth_lp_files_test.py
@@ -6,6 +6,10 @@ import hashlib
 import os
 import os.path
 
+sys.path.append( os.path.join( os.path.dirname(__file__), ".." ))
+
+import dbrecon
+
 TEST_CONFIG_FILE="config_test.ini"
 
 # We test by remaking the smallest of all the LP files
@@ -15,9 +19,21 @@ SMALL_STATE='29'
 SMALL_COUNTY='183'
 SMALL_TRACT='980000'
 
+
+
 def test_build_tract_lp():
     return 
 
 if __name__=="__main__":
-    call([sys.executable,'s3_pandas_synth_lp_files.py','--debug','--output','/tmp/model_29183980000.lp.gz',SMALL_STUSAB,SMALL_COUNTY,SMALL_TRACT,'--stdout'])
-
+    #call([sys.executable,'s3_pandas_synth_lp_files.py','--debug','--output','/tmp/model_29183980000.lp.gz',SMALL_STUSAB,SMALL_COUNTY,SMALL_TRACT,'--stdout'])
+    # Now compare the two files
+    f1 = dbrecon.dopen("/tmp/model_29183980000.lp.gz","r")
+    f2 = dbrecon.dopen("tests/model_29183980000.lp.gz","r")
+    while True:
+        line1 = f1.readline()
+        line2 = f2.readline()
+        if line1!=line2:
+            print("line1 != line2")
+        if line1=="": 
+            break
+    print("{} and {} are the same".format(f1.name,f2.name))

156ac392ccf48737d2d8fd4065cb2ede17cc5495
woode011 <jonathan.f.woodell@census.gov>
2019-08-16 09:53:05 -0400

    Fix for P20 and added P21

--- sf1_info_house.py
+++ sf1_info_house.py
@@ -98,31 +98,69 @@ class P20_Builder(Builder_HouseHold):
     def __init__(self, variables):
         self.map = OrderedDict(
             [
-                ("P020005", [default_HHSEX, range(0, 8), default_HISP, default_RACE, range(3, 8), [0, 1, 2],
+                ("P020005", [default_HHSEX, range(0, 6), default_HISP, default_RACE, range(3, 8), [0, 1, 2],
                              default_ELDERLY, default_MULTI]),
-                ("P020006", [default_HHSEX, range(0, 8), default_HISP, default_RACE, range(2, 8), [3],
+                ("P020006", [default_HHSEX, range(0, 6), default_HISP, default_RACE, range(2, 8), [3],
                              default_ELDERLY, default_MULTI]),
-                ("P020009", [[0], range(0, 8), default_HISP, default_RACE, range(2, 8), [19, 20, 21],
+                ("P020009", [[0], range(0, 6), default_HISP, default_RACE, range(2, 8), [19, 20, 21],
                              default_ELDERLY, default_MULTI]),
-                ("P020010", [[0], range(0, 8), default_HISP, default_RACE, range(2, 8), [22],
+                ("P020010", [[0], range(0, 6), default_HISP, default_RACE, range(2, 8), [22],
                              default_ELDERLY, default_MULTI]),
-                ("P020012", [[1], range(0, 8), default_HISP, default_RACE, range(2, 8), [19, 20, 21],
+                ("P020012", [[1], range(0, 6), default_HISP, default_RACE, range(2, 8), [19, 20, 21],
                              default_ELDERLY, default_MULTI]),
-                ("P020013", [[1], range(0, 8), default_HISP, default_RACE, range(2, 8), [22],
+                ("P020013", [[1], range(0, 6), default_HISP, default_RACE, range(2, 8), [22],
                              default_ELDERLY, default_MULTI]),
-                ("P020015", [default_HHSEX, range(0, 8), default_HISP, default_RACE, [1], [18],
+                ("P020015", [default_HHSEX, range(0, 6), default_HISP, default_RACE, [1], [18],
                              default_ELDERLY, default_MULTI]),
-                ("P020016", [default_HHSEX, range(0, 8), default_HISP, default_RACE, range(2, 8), [23],
+                ("P020016", [default_HHSEX, range(0, 6), default_HISP, default_RACE, range(2, 8), [23],
                              default_ELDERLY, default_MULTI])
             ]
         )
         # The reason we are doing this is because the next 8 are the same as the first 8 just a different
         # value for the HHAGE. So we use a ordered dict so we can safely loop through the items
         # and add them back when the changes variable with a new key.
+        temp_map = {}
         repeat_variables = ["P020020", "P020021", "P020024", "P020025", "P020027", "P020028", "P020030",
                             "P020031"]
         for index, (key, value) in enumerate(self.map.items()):
             current_value = deepcopy(value)
-            current_value[1] = [8]
-            self.map[repeat_variables[index]] = current_value
+            current_value[1] = [6, 7, 8]
+            temp_map[repeat_variables[index]] = current_value
+        self.map.update(temp_map)
+        super().__init__()
+
+class P21_Builder(Builder_HouseHold):
+
+    def __init__(self, variables):
+        self.map = OrderedDict(
+            [
+                ("P021003", [default_HHSEX, [0], default_HISP, default_RACE, default_SIZE, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 19, 20, 21, 22],
+                             default_ELDERLY, default_MULTI]),
+                ("P021004", [default_HHSEX, [1], default_HISP, default_RACE, default_SIZE, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 19, 20, 21, 22],
+                             default_ELDERLY, default_MULTI]),
+                ("P021005", [default_HHSEX, [2], default_HISP, default_RACE, default_SIZE, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 19, 20, 21, 22],
+                             default_ELDERLY, default_MULTI]),
+                ("P021006", [default_HHSEX, [3], default_HISP, default_RACE, default_SIZE, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 19, 20, 21, 22],
+                             default_ELDERLY, default_MULTI]),
+                ("P021007", [default_HHSEX, [4, 5], default_HISP, default_RACE, default_SIZE, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 19, 20, 21, 22],
+                             default_ELDERLY, default_MULTI]),
+                ("P021008", [default_HHSEX, [6], default_HISP, default_RACE, default_SIZE, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 19, 20, 21, 22],
+                             default_ELDERLY, default_MULTI]),
+                ("P021009", [default_HHSEX, [7], default_HISP, default_RACE, default_SIZE, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 19, 20, 21, 22],
+                             default_ELDERLY, default_MULTI]),
+                ("P021010", [default_HHSEX, [8], default_HISP, default_RACE, default_SIZE, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 19, 20, 21, 22],
+                             default_ELDERLY, default_MULTI])
+            ]
+        )
+        # The reason we are doing this is because the next 8 are the same as the first 8 just a different
+        # value for the HHAGE. So we use a ordered dict so we can safely loop through the items
+        # and add them back when the changes variable with a new key.
+        temp_map = {}
+        repeat_variables = ["P021012", "P021013", "P021014", "P021015", "P021016", "P021017", "P021018",
+                            "P021019"]
+        for index, (key, value) in enumerate(self.map.items()):
+            current_value = deepcopy(value)
+            current_value[5] = [12, 17, 18, 23]
+            temp_map[repeat_variables[index]] = current_value
+        self.map.update(temp_map)
         super().__init__()

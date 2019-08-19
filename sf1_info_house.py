09c53539a26db6aa9d2d6d021cc73332b6c73b67
woode011 <jonathan.f.woodell@census.gov>
2019-08-19 15:10:02 -0400

    Added support for a few more sets of tables

--- sf1_info_house.py
+++ sf1_info_house.py
@@ -1,4 +1,5 @@
 from collections import OrderedDict
+from string import ascii_uppercase
 from copy import deepcopy
 
 default_HHSEX = range(2)
@@ -26,7 +27,7 @@ class Builder_HouseHold:
 
 class P15_Builder(Builder_HouseHold):
 
-    def __init__(self, variables):
+    def __init__(self):
         self.map = {
             "P015001": [default_HHSEX, default_HHAGE, default_HISP, default_RACE, default_SIZE, default_HHTYPE,
                         default_ELDERLY, default_MULTI]
@@ -36,7 +37,7 @@ class P15_Builder(Builder_HouseHold):
 
 class P18_Builder(Builder_HouseHold):
 
-    def __init__(self, variables):
+    def __init__(self):
         self.map = {
             "P018003": [[0], default_HHAGE, default_HISP, default_RACE, [1], [18],
                         default_ELDERLY, [0]],
@@ -64,7 +65,7 @@ class P18_Builder(Builder_HouseHold):
 
 class P19_Builder(Builder_HouseHold):
 
-    def __init__(self, variables):
+    def __init__(self):
         self.map = {
             "P019004": [default_HHSEX, default_HHAGE, default_HISP, default_RACE, range(3, 8), [0, 1, 2],
                         default_ELDERLY, default_MULTI],
@@ -93,7 +94,7 @@ class P19_Builder(Builder_HouseHold):
 
 class P20_Builder(Builder_HouseHold):
 
-    def __init__(self, variables):
+    def __init__(self):
         self.map = OrderedDict(
             [
                 ("P020005", [default_HHSEX, range(0, 6), default_HISP, default_RACE, range(3, 8), [0, 1, 2],
@@ -129,7 +130,7 @@ class P20_Builder(Builder_HouseHold):
 
 class P21_Builder(Builder_HouseHold):
 
-    def __init__(self, variables):
+    def __init__(self):
         self.map = OrderedDict(
             [
                 ("P021003", [default_HHSEX, [0], default_HISP, default_RACE, default_SIZE, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 19, 20, 21, 22],
@@ -165,7 +166,7 @@ class P21_Builder(Builder_HouseHold):
 
 class P22_Builder(Builder_HouseHold):
 
-    def __init__(self, variables):
+    def __init__(self):
         self.map = OrderedDict(
             [
                 ("P022003", [default_HHSEX, [5, 6, 7, 8], default_HISP, default_RACE, [1], [18],
@@ -188,7 +189,7 @@ class P22_Builder(Builder_HouseHold):
 
 class P23_Builder(Builder_HouseHold):
 
-    def __init__(self, variables):
+    def __init__(self):
         self.map = OrderedDict(
             [
                 ("P023003", [default_HHSEX, [6, 7, 8], default_HISP, default_RACE, [1], [18],
@@ -209,9 +210,10 @@ class P23_Builder(Builder_HouseHold):
         self.map.update(temp_map)
         super().__init__()
 
+
 class P24_Builder(Builder_HouseHold):
 
-    def __init__(self, variables):
+    def __init__(self):
         self.map = OrderedDict(
             [
                 ("P024003", [default_HHSEX, [7, 8], default_HISP, default_RACE, [1], [18],
@@ -232,7 +234,192 @@ class P24_Builder(Builder_HouseHold):
         self.map.update(temp_map)
         super().__init__()
 
-class P25_Builder(Builder_HouseHold):
 
-    def __init__(self, variables):
-        pass
+class P31_Letter_Builder(Builder_HouseHold):
+
+    def __init__(self, table_name):
+        self.P31_Default = [default_HHSEX, default_HHAGE, -1, -1, default_SIZE, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 19, 20, 21, 22],
+                            default_ELDERLY, default_MULTI]
+        self.map = OrderedDict([])
+        self.race_for_table = default_RACE
+        self.hisp_for_table = default_HISP
+        self.table_variable = None
+
+        self.specific_P31_table(table_name)
+        self.build_map()
+
+    def build_table_variable_name(self, table_letter):
+        return f'P031{table_letter}001'
+
+    def specific_P31_table(self, table_name):
+        if table_name == 'P31A':
+            self.race_for_table = [0]
+            self.table_variable = self.build_table_variable_name('A')
+        elif table_name == 'P31B':
+            self.race_for_table = [1]
+            self.table_variable = self.build_table_variable_name('B')
+        elif table_name == 'P31C':
+            self.race_for_table = [3]
+            self.table_variable = self.build_table_variable_name('C')
+        elif table_name == 'P31D':
+            self.race_for_table = [2]
+            self.table_variable = self.build_table_variable_name('D')
+        elif table_name == 'P31E':
+            self.race_for_table = [4]
+            self.table_variable = self.build_table_variable_name('E')
+        elif table_name == 'P31F':
+            self.race_for_table = [5]
+            self.table_variable = self.build_table_variable_name('F')
+        elif table_name == 'P31G':
+            self.race_for_table = [6]
+            self.table_variable = self.build_table_variable_name('G')
+        elif table_name == 'P31H':
+            self.hisp_for_table = [1]
+            self.table_variable = self.build_table_variable_name('H')
+        elif table_name == 'P31I':
+            self.race_for_table = [0]
+            self.hisp_for_table = [0]
+            self.table_variable = self.build_table_variable_name('I')
+
+    def build_map(self):
+        current_default = deepcopy(self.P15_Default)
+        current_default[2] = self.hisp_for_table
+        current_default[3] = self.race_for_table
+        self.map[self.table_variable] = current_default
+
+
+class P15_Letter_Builder(Builder_HouseHold):
+
+    def __init__(self, table_name):
+        self.P15_Default = [default_HHSEX, default_HHAGE, -1, -1, default_SIZE, default_HHTYPE,
+                              default_ELDERLY, default_MULTI]
+        self.map = OrderedDict([])
+        self.race_for_table = default_RACE
+        self.hisp_for_table = default_HISP
+        self.table_variable = None
+
+        self.specific_P15_table(table_name)
+        self.build_map()
+
+    def build_table_variable_name(self, table_letter):
+        return f'P015{table_letter}001'
+
+    def specific_P15_table(self, table_name):
+        if table_name == 'P15A':
+            self.race_for_table = [0]
+            self.table_variable = self.build_table_variable_name('A')
+        elif table_name == 'P15B':
+            self.race_for_table = [1]
+            self.table_variable = self.build_table_variable_name('B')
+        elif table_name == 'P15C':
+            self.race_for_table = [3]
+            self.table_variable = self.build_table_variable_name('C')
+        elif table_name == 'P15D':
+            self.race_for_table = [2]
+            self.table_variable = self.build_table_variable_name('D')
+        elif table_name == 'P15E':
+            self.race_for_table = [4]
+            self.table_variable = self.build_table_variable_name('E')
+        elif table_name == 'P15F':
+            self.race_for_table = [5]
+            self.table_variable = self.build_table_variable_name('F')
+        elif table_name == 'P15G':
+            self.race_for_table = [6]
+            self.table_variable = self.build_table_variable_name('G')
+        elif table_name == 'P15H':
+            self.hisp_for_table = [1]
+            self.table_variable = self.build_table_variable_name('H')
+        elif table_name == 'P15I':
+            self.race_for_table = [0]
+            self.hisp_for_table = [0]
+            self.table_variable = self.build_table_variable_name('I')
+
+    def build_map(self):
+        current_default = deepcopy(self.P15_Default)
+        current_default[2] = self.hisp_for_table
+        current_default[3] = self.race_for_table
+        self.map[self.table_variable] = current_default
+
+
+class P26_Letter_Builder(Builder_HouseHold):
+
+    def __init__(self, table_name):
+        self.map = OrderedDict([])
+        self.race_for_table = default_RACE
+        self.hisp_for_table = default_HISP
+        self.table_variable = None
+        super().__init__()
+
+        self.specific_P26_table(table_name)
+        self.build_map()
+
+    def specific_P26_table(self, table_name):
+        if table_name == 'P26':
+            self.table_variable = ''
+        elif table_name == 'P26A':
+            self.race_for_table = [0]
+            self.table_variable = 'A'
+        elif table_name == 'P26B':
+            self.race_for_table = [1]
+            self.table_variable = 'B'
+        elif table_name == 'P26C':
+            self.race_for_table = [3]
+            self.table_variable = 'C'
+        elif table_name == 'P26D':
+            self.race_for_table = [2]
+            self.table_variable = 'D'
+        elif table_name == 'P26E':
+            self.race_for_table = [4]
+            self.table_variable = 'E'
+        elif table_name == 'P26F':
+            self.race_for_table = [5]
+            self.table_variable = 'F'
+        elif table_name == 'P26G':
+            self.race_for_table = [6]
+            self.table_variable = 'G'
+        elif table_name == 'P26H':
+            self.hisp_for_table = [1]
+            self.table_variable = 'H'
+        elif table_name == 'P26I':
+            self.race_for_table = [0]
+            self.hisp_for_table = [0]
+            self.table_variable = 'I'
+
+    def build_map(self):
+        if self.table_variable is None:
+            raise ValueError("P26 table letter did not get set.")
+        self.map = OrderedDict(
+            [
+                (f"P026{self.table_variable}003", [default_HHSEX, default_HHAGE, self.hisp_for_table, self.race_for_table, [2],
+                                                   [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 19, 20, 21, 22],
+                                                   default_ELDERLY, default_MULTI]),
+                (f"P026{self.table_variable}004", [default_HHSEX, default_HHAGE, self.hisp_for_table, self.race_for_table, [3],
+                                                   [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 19, 20, 21, 22],
+                                                   default_ELDERLY, default_MULTI]),
+                (f"P026{self.table_variable}005", [default_HHSEX, default_HHAGE, self.hisp_for_table, self.race_for_table, [4],
+                                                   [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 19, 20, 21, 22],
+                                                   default_ELDERLY, default_MULTI]),
+                (f"P026{self.table_variable}006", [default_HHSEX, default_HHAGE, self.hisp_for_table, self.race_for_table, [5],
+                                                   [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 19, 20, 21, 22],
+                                                   default_ELDERLY, default_MULTI]),
+                (f"P026{self.table_variable}007", [default_HHSEX, default_HHAGE, self.hisp_for_table, self.race_for_table, [6],
+                                                   [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 19, 20, 21, 22],
+                                                   default_ELDERLY, default_MULTI]),
+                (f"P026{self.table_variable}008", [default_HHSEX, default_HHAGE, self.hisp_for_table, self.race_for_table, [7],
+                                                   [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 19, 20, 21, 22],
+                                                   default_ELDERLY, default_MULTI])
+            ]
+        )
+        temp_map = {}
+        repeat_variables = [f"P026{self.table_variable}010", f"P026{self.table_variable}011", f"P026{self.table_variable}012", f"P026{self.table_variable}013",
+                            f"P026{self.table_variable}014", f"P026{self.table_variable}015", f"P026{self.table_variable}016"]
+        non_family = [default_HHSEX, default_HHAGE, self.hisp_for_table, self.race_for_table, -1, [12, 17, 18, 23],
+                      default_ELDERLY, default_MULTI]
+        for index, variable in enumerate(repeat_variables):
+            current_value = deepcopy(non_family)
+            current_value[4] = [index + 1]
+            temp_map[variable] = current_value
+        self.map.update(temp_map)
+
+# [default_HHSEX, default_HHAGE, default_HISP, default_RACE, default_SIZE, default_HHTYPE,
+#                         default_ELDERLY, default_MULTI]

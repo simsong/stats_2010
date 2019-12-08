5571954214e8cfa06da7e4aff1a6e70dc1279063
Simson L. Garfinkel <simsong@acm.org>
2019-12-08 09:55:33 -0500

    hhtype printing

--- hhtype/run_sugar.py
+++ hhtype/run_sugar.py
@@ -1,21 +1,34 @@
 #!/usr/bin/python
 #
-# run_sugar.py:
-#
-# run the sugar with the specified input file and optionally print all possible results
+"""run_sugar.py:
 
+Run sugar and with the specified input file and optionally print all possible results.
+
+Decode the output either with SUGAR or with our PYTHON implementation
+or with both (used to validate the python implementation, which is
+much faster.)
+
+If variables are defined with a $PRINT comment, we nicely format that.
+"""
 import os
 import sys
 import re
 from subprocess import Popen,PIPE,call
 from collections import defaultdict
 import time
+import logging
+import string
 
 SUGAR_PERL = './sugar-v2-3-3/bin/sugar'
 SUGAR_JAR  = './sugar-v2-3-3/bin/sugar-v2-3-3.jar'
+SUGAR_MAIN = 'jp.kobe_u.sugar.SugarMain'
 PICOSAT_SOLVER = '/opt/local/bin/picosat'
 GLUCOSE_SOLVER = './glucose_static'
 DEFAULT_SOLVER = PICOSAT_SOLVER
+
+DECODE_SUGAR = False
+DECODE_PYTHON = True
+
 
 # Our encodings
 SEXMAP = {"1":"M", "0":"F"}
@@ -40,14 +53,14 @@ class Hashabledict(dict):
         return False
         
         
+def validate_installation():
+    """Check files"""
+    for fn in [SUGAR_PERL,SUGAR_JAR]:
+        if not os.path.exists(fn):
+            raise FileNotFoundError(f"Cannot find sugar {fn}; download it from http://bach.istc.kobe-u.ac.jp/sugar/")
 
-### Check files
-for fn in [SUGAR_PERL,SUGAR_JAR]:
-    if not os.path.exists(fn):
-        raise FileNotFoundError(f"Cannot find sugar {fn}; download it from http://bach.istc.kobe-u.ac.jp/sugar/")
-
-if not os.path.exists(PICOSAT_SOLVER):
-    raise FileNotFoundError(f"Cannot find {PICOSAT_SOLVER}")
+    if not os.path.exists(PICOSAT_SOLVER):
+        raise FileNotFoundError(f"Cannot find {PICOSAT_SOLVER}")
 
 
 ################################################################
@@ -58,46 +71,18 @@ if not os.path.exists(PICOSAT_SOLVER):
 
 
 def is_cnf_file(fname):
+    """CNF (conjunctive normal form) output files are produced by DIMACS
+    solvers.  Validate that this is this a CNF file.
+    """
     with open(fname,"r") as f:
         return f.read(6)=="p cnf "
 
-def is_satisfied(fname):
+def is_satisfied_cnf(fname):
     with open(fname,"r") as f:
         for line in f:
             if "s SATISFIABLE" in line:
                 return True
         return False
-
-def tally_dimacs_file(dimacs_file):
-    assert is_cnf_file(dimacs_file)
-    with open(dimacs_file,"r") as f:
-        for line in f:
-            if line[0]=='c': continue
-            fields = line.strip().split(" ")
-            assert fields[0]=='p'
-            assert fields[1]=='cnf'
-            return (int(fields[2]),int(fields[3]))
-
-def run_solver(*,solver,cnffile,outfile):
-    assert os.path.exists(cnffile)
-    assert not os.path.exists(outfile)
-    if "picosat" in solver:
-        cmd = [solver,cnffile,'-o',outfile]
-    elif "glucose" in solver:
-        cmd = [solver,cnffile,outfile]
-    else:
-        raise RuntimeError("Unknown solver: {}".format(solver))
-    print(" ".join(cmd))
-    p = Popen(cmd,stdout=PIPE,stderr=PIPE)
-    (out,err) = p.communicate()
-    assert os.path.exists(cnffile)
-    assert os.path.exists(outfile)
-    if is_satisfied(outfile):
-        return 
-    solver_out = out.decode('utf-8'); del out
-    solver_err = err.decode('utf-8'); del err
-    raise RuntimeError("solver failed. ret={} out='{}' err='{}'".
-                       format(p.returncode,solver_out,solver_err))
 
 ################################################################
 ###
@@ -105,14 +90,13 @@ def run_solver(*,solver,cnffile,outfile):
 ###
 ################################################################
 
-
 def sugar_encode_csp(*,cspfile,cnffile,mapfile):
     """Run sugar to make an output file"""
     assert os.path.exists(cspfile)
     assert not os.path.exists(cnffile)
     assert not os.path.exists(mapfile)
-    cmd = ['java','-cp',SUGAR_JAR,'jp.kobe_u.sugar.SugarMain','-v', '-encode', cspfile, cnffile, mapfile]
-    print(" ".join(cmd))
+    cmd = ['java','-cp',SUGAR_JAR,SUGAR_MAIN,'-v', '-encode', cspfile, cnffile, mapfile]
+    logging.info("%s"," ".join(cmd))
     p = Popen(cmd,stdout=PIPE,stderr=PIPE)
     (out,err) = p.communicate()
     sugar_out = out.decode('utf-8'); del out
@@ -125,7 +109,7 @@ def sugar_encode_csp(*,cspfile,cnffile,mapfile):
     
 def sugar_decode_picosat_out(outdata,mapfile):
     cmd = ['java','-cp',SUGAR_JAR,'jp.kobe_u.sugar.SugarMain','-decode','/dev/stdin',mapfile]
-    print(" ".join(cmd))
+    logging.info("%s"," ".join(cmd))
     (out,err) = Popen(cmd,stdin=PIPE,stdout=PIPE,stderr=PIPE).communicate(outdata.encode('utf-8'))
     out = out.decode('utf-8')
     err = err.decode('utf-8')
@@ -236,13 +220,12 @@ def extract_vars_from_sugar_decode(outdata):
         line = line.strip()
         if len(line)==0: continue
         if line[0]=='s' and line!='s SATISFIABLE':
-            print(outdata)
+            logging.error(outdata)
             raise RuntimeError("Constraints not satisfied; line={}".format(line))
         if line[0]=='a' and "\t" in line:
             (var,val) = line[2:].split("\t")
             vars[var] = val
     return vars
-
 
 def sugar_decode_picostat_and_extract_satvars(lines,mapfile):
     return extract_vars_from_sugar_decode( sugar_decode_picosat_out( "\n".join(lines), mapfile))
@@ -264,38 +247,97 @@ def picosat_get_next_solution(path):
         if lines:
             yield(lines)
             
+def sparse(line):
+    """A tiny little and possibly broken s-expression parser"""
+    stack = []                  # current stack
+    ret   = []                  # what we are returning
+    atom  = None                # current atom being built
+    
+    for ch in line:
+        if ch==';':
+            break
+        if ch=='(':
+            stack.append([])
+            atom = None
+            continue
+        if ch==')':
+            ret.append(stack.pop())
+            atom = None
+            if len(stack)==0:
+                return ret[0]
+            continue
+        if ch in string.whitespace:
+            atom = None
+            continue
+        if atom is None:
+            atom = ch
+            stack[-1].append(ch)
+        else:
+            stack[-1].append( stack[-1].pop() + ch)
+    return ret[0]
+    
+            
+class PicosatPrinter:
+    """Use picosat to find all of the solutions"""
 
-def parse_picosat_all_file(path):
-    print("parsing picosat all file {}".format(path))
-    total_solutions = 0
-    distinct_solutions = 0
-    seen = set()
-    ctr = 0
-    USE_SUGAR = False
-    USE_PYTHON = True
-    for lines in picosat_get_next_solution(path):
-        total_solutions += 1
-        print("#{}:".format(total_solutions),end='')
-        # We can use either the python decoder or the sugar decoder.
-        # If we use both, we can compare them.
-        if USE_PYTHON:
-            (mapvars,ssatvars) = python_decode_picosat_and_extract_satvars(
-                solver_output_lines=lines,mapfile=args.map)
-            print(ssatvars)
-            seen.add(ssatvars)
-        
-        if USE_SUGAR:
-            satvars = sugar_decode_picostat_and_extract_satvars(lines,args.map)
-            if not USE_PYTHON:
-                print(satvars)
-            seen.add(satvars)
+    def __init__(self):
+        self.printvars  = set()
+        pass
 
-        if USE_PYTHON and USE_SUGAR:
-            assert ssatvars==satvars
+    def get_printvars(self,path):
+        for line in open(path):
+            if "$PRINT" in line:
+                s = sparse(line)
+                if s[0] in ['int','bool']:
+                    self.printvars.add(s[1])
 
-    for (ct,satvars) in enumerate(sorted(seen),1):
-        print(f"sugar solution {ct} / {total_solutions}: {satvars}")
+    def parse_picosat_all_solutions(self,path,mapfile):
+        logging.info("parsing all picosat solutions in %s",path)
+        total_solutions = 0
+        distinct_solutions = 0
+        seen = dict()
+        ctr = 0
+        solutions = []
+        for lines in picosat_get_next_solution(path):
+            # We can use either the python decoder or the sugar decoder.
+            # If we use both, we can compare them.
+            if DECODE_PYTHON:
+                (mapvars,ssatvars) = python_decode_picosat_and_extract_satvars(solver_output_lines=lines,mapfile=mapfile)
+                solutions.append(ssatvars)
 
+            if DECODE_SUGAR:
+                satvars = sugar_decode_picostat_and_extract_satvars(lines,mapfile)
+                if DECODE_PYTHON:
+                    assert ssatvars==satvars
+                else:
+                    solutions.append(satvars)
+
+        # Sort solutions according to our variables
+        solutions.sort()
+        # Build a map of each solution to its solution number
+        seen = {satvar:ct for (ct,satvar) in enumerate(solutions)}
+
+        if not self.printvars:
+            print("No $PRINT variables specified. Printing all solutions.")
+            for (ct,satvars) in enumerate(solutions,1):
+                print(f"picosat solution #{ct}: {satvars}")
+        else:
+            print("Printing just these varilabes in solutions: ",self.printvars)
+            eseen = defaultdict(list)
+            for (ct,satvars) in enumerate(solutions,1):
+                # extract the printing vars from this solution
+                extracted = Hashabledict()
+                for var in self.printvars:
+                    extracted[var] = satvars[var]
+
+                # If we have seen this before, indicate that two solutions have the same variables
+                print(f"# {ct}: {extracted}")
+                if extracted in eseen:
+                    print(f"  DEGENERATE PRINTED VARIABLES")
+                    for deg in eseen[extracted]:
+                        print(f"   #{deg}: {solutions[deg]}")
+                    print(f"   #{ct}: {solutions[ct]}")
+                eseen[extracted].append(ct)
 
 
 if __name__=="__main__":
@@ -305,84 +347,83 @@ if __name__=="__main__":
     parser.add_argument("--cnf",default="constraints.cnf",help="specify cnf file")
     parser.add_argument("--parseout",
                         help="Parse the variables out of an .out file. A map file must be specified")
-    parser.add_argument("--parseall",help='Evaluate output file of the picosat --all')
     parser.add_argument("--solver",default="picosat")
     parser.add_argument("--picosat_out",help='picosat output file',default='constraints.out')
-    parser.add_argument("--map",help="Specify map file",default='constraints.map')
+    parser.add_argument("--mapfile",help="Specify map file",default='constraints.map')
     parser.add_argument("--all",help="Compute all possible solutions; store results in ALL")
+    parser.add_argument("--parseall",help='Just Evaluate output file of the picosat --all (you also need a --mapfile)')
     parser.add_argument("--decode",help="test the decode function")
     parser.add_argument("--define",help="specify a #define to CPP")
     parser.add_argument("--sugar_output",help="specify sugar output",default='constraints.sugar.out')
-    parser.add_argument("--csp", help="CSP File")
+    parser.add_argument("--tmpcsp", help="Temp CSP File (after running cpp)")
     parser.add_argument("problem",default="hhtype.csp",help="specify the problem CSP")
     args = parser.parse_args()
 
-    if not args.csp:
-        args.csp = args.problem+".tmp"
+    validate_installation()
+
+    if not args.tmpcsp:
+        args.tmpcsp = args.problem+".tmp"
 
     if args.parseout:
         out = open(args.parseout,"r").read()
         (mapvars,satvars,counter) = python_decode_picosat_and_extract_satvars(
-            solver_output_lines=out.split("\n"), 
-            mapfile=args.map) 
+            solver_output_lines=out.split("\n"), mapfile=args.mapfile) 
         print(parse_vars_to_printable(satvars))
         exit(1)
-
-    if args.parseall:
-        parse_picosat_all_file(args.parseall)
-        exit(0)
 
     if args.decode:
         decode(open(args.decode,"r").read())
         exit(0)
 
+    if not args.parseall:
+        # Run the pre-processor and remove the files that begin with a '#'
+        cmd = ['cpp',args.problem]
+        (out,err) = Popen(cmd,stdout=PIPE).communicate()
+        with open( args.tmpcsp, "w") as f:
+            for line in out.decode('utf-8').split("\n"):
+                if line.startswith('#'):
+                    continue
+                f.write(line)
+                f.write("\n")
 
-    # Run the pre-processor and remove the files that begin with a '#'
-    cmd = ['cpp',args.problem]
-    (out,err) = Popen(cmd,stdout=PIPE).communicate()
-    with open( args.csp,"w") as f:
-        for line in out.decode('utf-8').split("\n"):
-            if line.startswith('#'):
-                continue
-            f.write(line)
-            f.write("\n")
+        # Run sugar, which runs the solver
+        if args.all:
+            args.solver += " --all"
+        tmp = args.cnf.replace(".cnf","")
 
-    # Run sugar, which runs the solver
-    if args.all:
-        args.solver += " --all"
-    tmp = args.cnf.replace(".cnf","")
+        # Here is what the Java looks like
+        # java  -cp './sugar-v2-3-3/bin/sugar-v2-3-3.jar' jp.kobe_u.sugar.SugarMain -vv  -encode 'constraints.csp' 'constraints.cnf' 'constraints.map'
 
-    # Here is what the Java looks like
-    # java  -cp './sugar-v2-3-3/bin/sugar-v2-3-3.jar' jp.kobe_u.sugar.SugarMain -vv  -encode 'constraints.csp' 'constraints.cnf' 'constraints.map'
+        cmd = ['perl',SUGAR_PERL,
+               '-jar',SUGAR_JAR,
+               '-vv',
+               '-solver',args.solver,'-keep','-tmp','constraints',
+               '-output',args.picosat_out,
+               args.tmpcsp]
 
-    cmd = ['perl',SUGAR_PERL,
-           '-jar',SUGAR_JAR,
-           '-vv',
-           '-solver',args.solver,'-keep','-tmp','constraints',
-           '-output',args.picosat_out]
-
-    cmd += [args.csp]
-    print(" ".join(cmd))
-    t0 = time.time()
-    p = Popen(cmd,stdout=PIPE,stderr=PIPE,encoding='utf-8')
-    (sugar_out,sugar_err) = p.communicate()
-    t1 = time.time()
-    if p.returncode!=0:
-        raise RuntimeError("sugar returned {}. out='{}' err='{}'".
-                           format(p.returncode,sugar_out,sugar_err))
-        exit(1)
-    print("sugar run time: {:.4} sec".format(t1-t0))
-    for line in sugar_out.split("\n"):
-        if ("ERROR" in line) and ("SOLUTIONS" not in line):
-            print("Error in running sugar:")
-            print(line)
+        print(" ".join(cmd))
+        t0 = time.time()
+        p = Popen(cmd,stdout=PIPE,stderr=PIPE,encoding='utf-8')
+        (sugar_out,sugar_err) = p.communicate()
+        t1 = time.time()
+        if p.returncode!=0:
+            raise RuntimeError("sugar returned {}. out='{}' err='{}'".
+                               format(p.returncode,sugar_out,sugar_err))
             exit(1)
+        print("sugar run time: {:.4} sec".format(t1-t0))
+        for line in sugar_out.split("\n"):
+            if ("ERROR" in line) and ("SOLUTIONS" not in line):
+                print("Error in running sugar:")
+                print(line)
+                exit(1)
     
-    #print(sugar_out)
-    #print(sugar_err)
-
-    if args.all:
-        parse_picosat_all_file(args.picosat_out)
+    if args.all or args.parseall:
+        pp = PicosatPrinter()
+        pp.get_printvars(args.problem)
+        if args.parseall:
+            pp.parse_picosat_all_solutions(args.parseall,args.mapfile)
+        else:
+            pp.parse_picosat_all_solutions(args.picosat_out,args.mapfile)
         exit(0)
 
     # keep a copy of the sugar output

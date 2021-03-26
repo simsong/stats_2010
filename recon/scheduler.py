54242e8f4e322f947a29608e4a79dd03e7c62273
Simson L. Garfinkel <simson.l.garfinkel@census.gov>
2021-03-26 16:14:23 -0400

    every select just became a join

--- recon/scheduler.py
+++ recon/scheduler.py
@@ -314,7 +314,7 @@ def run(auth):
             cmd = f"""
                 SELECT t.stusab,t.county,count(*) as tracts,sum(g.pop100) as pop
                 FROM {REIDENT}tracts t LEFT JOIN {REIDENT}geo g ON (t.stusab=g.stusab and t.county=g.county and t.tract=g.tract)
-                WHERE (t.lp_end IS NULL) AND (t.hostlock IS NULL) AND (g.sumlev='140') and (pop>0)
+                WHERE (t.lp_end IS NULL) AND (t.hostlock IS NULL) AND (g.sumlev='140') and (pop100>0)
                 GROUP BY t.state,t.county
                 ORDER BY pop {direction} LIMIT 1
                 """.format()
